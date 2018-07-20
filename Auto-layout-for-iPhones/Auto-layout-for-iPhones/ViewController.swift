41352f846a2035fbca8d2dcf2840a67847819a3a
Krystyna Kruchkovska <kruchok883@gmail.com>
2018-07-20 19:40:16 +0300

    first commit

---
+++ Auto-layout-for-iPhones/Auto-layout-for-iPhones/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Auto-layout-for-iPhones
+//
+//  Created by Mac on 7/20/18.
+//  Copyright © 2018 CO.KrystynaKruchcovska. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
